f83a141a514cee49ebb50bed3a09335e75b4caf1
LucidBrot <eric@mink.li>
2021-10-24 00:11:32 +0200

    parsing in various ways

--- jedi/src/main.rs
+++ jedi/src/main.rs
@@ -7,6 +7,7 @@ use anyhow;
 use std::convert::TryFrom;
 use std::io;
 use thiserror::Error;
+#[macro_use] extern crate text_io;
 
 /// a JediPos can either be a start or end position of a Jedi interval.
 trait JediPos {
@@ -65,15 +66,33 @@ fn testcase(m: u32, n: u16, workvec: Vec<Box<JediStart>>) {
 // Also, would a Box<dyn Error> actually be the way to go? Box is on the heap, right?
 // One Alternative seems to be using the thiserror crate: https://stackoverflow.com/a/58337971/2550406
 fn main() -> Result<(), MainErrors> {
+    // This function uses multiple ways to parse, as an exercise for myself.
+    // see answers here: https://stackoverflow.com/a/33631589/2550406
+    // Ways I've found:
+    // Way1: read line, trim, parse, ?
+    // Way2: let [m, n] = match stuff
+    //          this has verbose but explicit error handling
+    // Way3: A one-liner that uses map to parse after splitting, then collects the contents and
+    //          uses a questionmark to get the result out.
+    //          https://stackoverflow.com/a/33630950/2550406
+    // Way4: using the macros read! and scan! from text_io crate
+    //          https://stackoverflow.com/a/33631589/2550406
+    // Way5: if let [Ok(m), Ok(n)] = & split map collect
+    //          https://stackoverflow.com/a/33631317/2550406
+    // 
+
     println!("Hello, world!");
 
     // read from stdin
     let reader = io::stdin();
+    /*
     let mut input_text = String::new();
     reader
         .read_line(&mut input_text)
         .expect("failed to read line");
     let num_testcases = input_text.trim().parse::<u8>()?;
+    */
+    let num_testcases: u8 = read!();
     println!("Parsed num_testcases {}", num_testcases);
 
     // for each testcase
@@ -83,8 +102,8 @@ fn main() -> Result<(), MainErrors> {
         let n: u16;
         let mut m_n_str = String::new();
         reader.read_line(&mut m_n_str).expect("failed to read line");
+        /*
         // split on space into two numbers
-        // TODO: annotate this in the documentation as magic to look at later
         let [m, n] = match <[&str; 2]>::try_from(
             m_n_str.split_whitespace().take(2).collect::<Vec<&str>>(),
         ) {
@@ -95,6 +114,8 @@ fn main() -> Result<(), MainErrors> {
         };
         let m = m.trim().parse::<u16>()?;
         let n = n.trim().parse::<u32>()?;
+        */
+        scan!("{} {}", m, n);
         println!("Parsed m,n: {}, {}", m, n);
 
         // read all the jedi into a vec
@@ -103,8 +124,11 @@ fn main() -> Result<(), MainErrors> {
             // read line containing a and b
             let mut a_b_str = String::new();
             reader.read_line(&mut a_b_str).expect(&format!("failed to read line for {} jedi", i));
-            let a_b: Vec<u32> = a_b_str.split_whitespace().map(|x| x.parse::<u32>())
-                .collect::<Result<Vec<u32>,_>>()?;
+            // apparently, using [..] after a vec is a trick to get the stuff out
+            if let [a, b] = a_b_str.split_whitespace().map(|x| x.parse::<u32>())
+                .collect::<Result<Vec<u32>,_>>()?[..] {
+                println!("Jedi {i}: ({a}, {b})", i=i, a=a, b=b);
+            }
 
         }
     }
